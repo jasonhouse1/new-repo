017c0e2f94c4d258e4ffcc0cc553ce38190eeac5
Tony <411631327@o365.tku.edu.tw>
2024-10-30 04:08:01 +0800

    Initial commit

--- 1024.ts
+++ 1024.ts
@@ -1,37 +1,25 @@
+let a = 1;
 
-var a =1;
 console.log(a);
 
-let b:number;
+let b: number;
 b = 2;
 console.log(b);
 
-let c:number = 123
+let c = 123;
 console.log(c);
-
-// 迴圈
-
-for (let i = 0; i<3; i++){
-    console.log("迴圈:" +i);
-}
-console.log("結束迴圈 i 的值是:" + 3);
-
-//迴圈
- 
 for (let i = 0; i<3; i++){
     console.log("迴圈:" + i);
 }
 console.log("結束迴圈 i的值是:" + 3);
  
-// 迴圈
  
 for (let i = 0; i < 10; i++) {
     console.log("迴圈次數：" + i);
   }
   console.log("迴圈結束，但此行會報錯，因為 'i' 已在區塊外無效。");
   console.log("由於上一行的錯誤，程式執行中斷，這一行不會被印出。");
-
-// 多重迴圈
+ 
  
 for (let i = 1; i < 10; i++) {
     for (let j = 1; j < 10; j++) {
